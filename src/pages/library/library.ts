0849c260a7fee3ea7fa8ae7a61677fdcd7e890e1
Prasanta Bose <prasantabose1310@gmail.com>
2018-07-23 10:49:29 +0530

    Interface used for quotes

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,13 +1,14 @@
 import { Component , OnInit } from '@angular/core';
 import quotes from '../../data/quotes';
+import { Quote } from '../../data/quote.interface';
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit {
-	quoteCollection:{ category:string , quotes:{ id:string,person:string,text:string }[] , icon:string }[];
+	quoteCollection:{ category:string , quotes:Quote[] , icon:string }[];
+	//quoteCollection:{ category:string , quotes:{ id:string,person:string,text:string }[] , icon:string }[];
 	ngOnInit(){
 		this.quoteCollection = quotes;
-		console.log(this.quoteCollection);
 	}
 }
